ed2bca9a36eb6554b0b338fb302416d06d0b58a9
YenusPutra <39011316+YenusPutra@users.noreply.github.com>
2025-10-14 16:23:15 +0700

    revise staticfiles

--- Lonergarden_site/settings.py
+++ Lonergarden_site/settings.py
@@ -95,14 +95,12 @@ WSGI_APPLICATION = 'Lonergarden_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'LonergardenHotelDB',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'admin',
-        # 'HOST': 'localhost',
-        # 'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'LonergardenHotelDB',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
