08532b59e984b51d9130c80bd8f18167dc4f00ff
YenusPutra <39011316+YenusPutra@users.noreply.github.com>
2025-10-14 18:50:15 +0700

    Case Correction

--- Lonergarden_site/settings.py
+++ Lonergarden_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!@*b2aeb)0cv3pv+ybbkuw9pk#h-#7nd81wu)0!b#ykmfx0b7-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
