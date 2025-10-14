d7fbca96c8080c7b2c1586630e50102daa607392
YenusPutra <39011316+YenusPutra@users.noreply.github.com>
2025-10-14 19:13:02 +0700

    fix database coding

--- Lonergarden_site/settings.py
+++ Lonergarden_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,14 +95,18 @@ WSGI_APPLICATION = 'Lonergarden_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'LonergardenHotelDB',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'LonergardenHotelDB',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'admin',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
+    'default': dj_database_url.config(
+        default='postgres://postgres:admin@localhost:5432/LonergardenHotelDB', #// Fallback for local testing
+        conn_max_age=600
+    )
 }
 
 
